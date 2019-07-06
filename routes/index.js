ebac882d34423d0445d487b0a97e76b80db0ea1e
WWZ <zeing@outlook.com>
2019-07-06 19:40:15 +0700

    chore: adjust code

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ const line = require('@line/bot-sdk');
 const router = Router();
 // create LINE SDK config from env variables
 const config = {
+  // channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  // channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
 /* GET index page. */
-router.get('/', (req, res) => {
+router.get('/callback', (req, res) => {
   res.json({
     title: 'Express'
   });
