f2ef63cf06f9d9fd2aad0e1d55d5cb895a770824
WWZ <zeing@outlook.com>
2019-07-06 19:25:23 +0700

    feat: init linebot

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,49 @@
 const { Router } = require('express');
+const line = require('@line/bot-sdk');
 
 const router = Router();
+// create LINE SDK config from env variables
+const config = {
+
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+router.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => {
+        res.json(result)
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+      });
+});
+
+function handleMessageEvent(event) {
+  let msg = {
+    type: 'text',
+    text: 'สวัสดีครัช'
+  };
+
+  return client.replyMessage(event.replyToken, msg);
+}
+
+
+function handleEvent(event) {
+
+  console.log(event);
+  if (event.type === 'message' && event.message.type === 'text') {
+    handleMessageEvent(event);
+  } else {
+    return Promise.resolve(null);
+  }
+}
+
 
 /* GET index page. */
 router.get('/', (req, res) => {
