63bc8902ccb95fb7584cbae915773c581076fa3d
Witthawin Sirisiwaphak <eing.wi@gettgo.com>
2020-06-07 15:23:56 +0700

    update key

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ const config = {
 const client = new line.Client(config);
 
 const clientTwitter = new Twitter({
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  access_token_key: process.env.access_token_key,
-  access_token_secret: process.env.access_token_secret
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 // webhook callback
